de38f264f7075f53dca773cfbc4ba54bca63a5ca
vikas236 <saivikas461@gmail.com>
2024-09-12 02:37:27 +0530

    add basic node server code

--- server.js
+++ server.js
@@ -3,13 +3,27 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
+const { Pool } = require("pg");
+require("dotenv").config();
 
 // Middleware to handle JSON requests
 app.use(express.json());
 
+// Create a new Pool instance
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL,
+});
+
 // Basic route to handle GET requests
-app.get("/", (req, res) => {
-  res.send("Hello, There!!!");
+app.get("/", async (req, res) => {
+  const client = await pool.connect();
+
+  // Run a simple query
+  const result = await client.query("SELECT * FROM user_management");
+  res.send(result.rows);
+
+  // Release the client back to the pool
+  client.release();
 });
 
 // Sample route to demonstrate handling a POST request
